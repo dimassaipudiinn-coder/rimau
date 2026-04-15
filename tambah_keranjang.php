ec55098b722e133d04308299324d5b2603e59a95
QinxxTzy <otot68333@gmail.com>
2026-04-15 10:46:39 +0700

    Update tambah_keranjang.php

--- tambah_keranjang.php
+++ tambah_keranjang.php
@@ -6,29 +6,52 @@ if (!isset($_SESSION['cart'])) {
     $_SESSION['cart'] = [];
 }
 
-$produk_id = $_POST['produk_id'];
-$qty = $_POST['qty'];
+/* ===== VALIDATION INPUT ===== */
+$produk_id = isset($_POST['produk_id']) ? (int)$_POST['produk_id'] : 0;
+$qty       = isset($_POST['qty']) ? (int)$_POST['qty'] : 1;
 
-// ambil data produk dari database
-$result = mysqli_query($conn, "SELECT * FROM produk WHERE id='$produk_id'");
-$produk = mysqli_fetch_assoc($result);
-
-if ($produk) {
-
-    // kalau produk sudah ada di cart, update qty
-    if (isset($_SESSION['cart'][$produk_id])) {
-        $_SESSION['cart'][$produk_id]['qty'] += $qty;
-    } else {
-        $_SESSION['cart'][$produk_id] = [
-            "id" => $produk['id'],
-            "nama" => $produk['nama_produk'],
-            "harga" => $produk['harga'],
-            "qty" => $qty
-        ];
-    }
-
+if ($produk_id <= 0 || $qty <= 0) {
+    header("Location: produk.php");
+    exit;
 }
 
-header("Location: keranjang.php");
+/* ===== AMBIL DATA PRODUK ===== */
+$produk_id_safe = mysqli_real_escape_string($conn, $produk_id);
+
+$result = mysqli_query($conn, "SELECT * FROM produk WHERE id='$produk_id_safe'");
+$produk = mysqli_fetch_assoc($result);
+
+if (!$produk) {
+    header("Location: produk.php");
+    exit;
+}
+
+/* ===== CEK STOK ===== */
+if ($qty > (int)$produk['stok']) {
+    $qty = (int)$produk['stok'];
+}
+
+/* ===== ADD / UPDATE CART ===== */
+if (isset($_SESSION['cart'][$produk_id])) {
+
+    $_SESSION['cart'][$produk_id]['qty'] += $qty;
+
+} else {
+
+    $_SESSION['cart'][$produk_id] = [
+        "id"    => $produk['id'],
+        "nama"  => $produk['nama_produk'],
+        "harga" => $produk['harga'],
+        "qty"   => $qty
+    ];
+}
+
+/* ===== OPTIONAL: CLEAN ZERO STOCK ITEM ===== */
+if ($_SESSION['cart'][$produk_id]['qty'] <= 0) {
+    unset($_SESSION['cart'][$produk_id]);
+}
+
+/* ===== RETURN ===== */
+header("Location: keranjang.php?status=success");
 exit;
 ?>
